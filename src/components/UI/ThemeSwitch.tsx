e1d408a8dd6e26180309e8753bdac37d42945677
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2025-10-17 12:11:12 +0300

    feat(theme): create animated switch component for theme toggle

---
+++ src/components/UI/ThemeSwitch.tsx
@@ -0,0 +1,27 @@
+import { Sun, Moon } from "lucide-react";
+
+type ThemeSwitchProps = {
+  isDarkMode: boolean;
+  handleToggleTheme: () => void;
+};
+
+export default function ThemeSwitch({ isDarkMode, handleToggleTheme }: ThemeSwitchProps) {
+  return (
+    <button
+      onClick={handleToggleTheme}
+      aria-label="Toggle theme"
+      className="relative inline-flex h-8 w-14 p-1 items-center rounded-full transition-colors duration-200 outline outline-main-purple hover:outline-primary-foreground cursor-pointer bg-secondary-background">
+      {/* This is the moving "knob" */}
+      <div
+        className={`inline-block h-6 w-6 transform rounded-full bg-background shadow-lg transition-transform duration-300 ${
+          isDarkMode ? "translate-x-6" : "translate-x-0"
+        }`}>
+        {isDarkMode ? (
+          <Moon className="h-full w-full p-1 text-primary-foreground" />
+        ) : (
+          <Sun className="h-full w-full p-1 text-yellow-500" />
+        )}
+      </div>
+    </button>
+  );
+}
