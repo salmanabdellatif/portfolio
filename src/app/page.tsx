fbe8fa3c761f7525fecf54d07f2418222de47fa1
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2025-10-19 22:36:15 +0300

    add new sections to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Hero from "@/components/home/Hero";
+import About from "@/components/home/About";
+import Work from "@/components/home/Work";
+import Projects from "@/components/home/Projects";
+import Education from "@/components/home/Education";
+import Contact from "@/components/home/Contact";
 
 export default function Home() {
   return (
-    <div>
-      <Hero />
-    </div>
+    <>
+      <About />
+      <Work />
+      <Projects />
+      <Education />
+      <Contact />
+    </>
   );
 }
