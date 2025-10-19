22999acbc960320e48079d47a22c4da65f9b953a
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2025-10-19 22:36:30 +0300

    create projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,17 @@
+import { projects } from "@/data/project";
+import ProjectCard from "@/components/ProjectCard";
+
 export default function ProjectsPage() {
-  return <div>Projects Page</div>;
+  return (
+    <div className="py-6">
+      <h2 className="text-3xl font-medium text-primary-foreground">
+        <span className="text-main-purple">*</span>projects
+      </h2>
+      <div className="mt-12 grid grid-cols-1 gap-4">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} {...project} />
+        ))}
+      </div>
+    </div>
+  );
 }
