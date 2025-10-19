69627be2ae4bd59b1d24187d2dcdc8258fe5bc57
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2025-10-19 22:34:58 +0300

    add temp projects data

---
+++ src/data/project.ts
@@ -0,0 +1,26 @@
+export type Project = {
+  title: string;
+  description: string;
+  technologies: string[];
+  liveDemoUrl?: string;
+  githubUrl: string;
+};
+
+export const projects: Project[] = [
+  {
+    title: "Cooming Soon",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti quas maxime soluta sunt earum expedita, beatae esse repudiandae non porro ullam, dolore perspiciatis similique quasi fugiat? Possimus soluta sed atque!",
+    technologies: ["HTML", "CSS", "JavaScript", "HTML", "CSS", "JavaScript"],
+    liveDemoUrl: "#",
+    githubUrl: "#",
+  },
+  {
+    title: "Cooming Soon",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti quas maxime soluta sunt earum expedita, beatae esse repudiandae non porro ullam, dolore perspiciatis similique quasi fugiat? Possimus soluta sed atque!",
+    technologies: ["HTML", "CSS", "JavaScript", "HTML", "CSS", "JavaScript"],
+    liveDemoUrl: "#",
+    githubUrl: "#",
+  },
+];
