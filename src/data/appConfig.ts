dce9e164348568294385c4ff465686888aa4e624
salmanabdellatif <70966096+salmanabdellatif@users.noreply.github.com>
2025-10-19 22:35:09 +0300

    add app config

---
+++ src/data/appConfig.ts
@@ -0,0 +1,6 @@
+export const appConfig = {
+  githubURL: "https://github.com/salmanabdellatif",
+  linkedInURL: "https://www.linkedin.com/in/salmanabdellatif/",
+  uniURL: "https://w3.sdu.edu.tr/",
+  email: "devsalmn@gmail.com",
+};
